a4f495f08ca35728674737d143a33725d58aef9b
TanishaSahu03 <sahutanisha045@gmail.com>
2025-04-07 15:00:37 +0530

    For first pull

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Ctrl+Enter to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+print("This is first pull")
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
